f94c082400dff029ec1ec4b14acae1281d85f094
AlphaKeks <alphakeks@dawn.sh>
2023-02-21 15:13:56 +0100

    fetch maps from spreadsheet

---
+++ migrations/20230205_maprelease.sql
@@ -0,0 +1,26 @@
+
+			INSERT INTO maps
+			  (id, map_id, stage, kzt, kzt_difficulty, skz, skz_difficulty, vnl, vnl_difficulty)
+			VALUES
+			
+			  (0, "kz_apiisnotresponding", 0, true, 52344340, 0, 0, "", ""),
+			  (1, "kz_cf_slide", 0, true, 41263092, 0, 0, "", ""),
+			  (2, "kz_desolate", 0, true, 75278696, 0, 0, "", ""),
+			  (3, "kz_itz_updown", 0, true, 140470496, 0, 0, "", ""),
+			  (4, "kz_tangent", 0, true, 172930632, 0, 0, "", ""),
+			  (5, "kz_tense", 0, true, 37122636, 0, 0, "", ""),
+			  (6, "kz_acores", 0, true, 131979212, 0, 0, "", ""),
+			  (7, "kz_hoist_fix", 0, true, 97855972, 0, 0, "", ""),
+
+			INSERT INTO courses
+			  (id, map_id, stage, kzt, kzt_difficulty, skz, skz_difficulty, vnl, vnl_difficulty)
+			VALUES
+			
+			  (0, 0, 0, true, 1, true, 1, true, 1),
+			  (0, 1, 0, true, 3, true, 3, false, 3),
+			  (0, 2, 0, true, 2, true, 2, true, 2),
+			  (0, 3, 0, true, 4, true, 4, false, 4),
+			  (0, 4, 0, true, 3, true, 3, false, 3),
+			  (0, 5, 0, true, 7, true, 7, false, 7),
+			  (0, 6, 0, true, 2, true, 2, true, 2),
+			  (0, 7, 0, true, 5, true, 5, false, 5),
